f980d2bb7c2b2cb9bcf3a7bf63e526b43eed3b47
Ross Karchner <rosskarchner@gmail.com>
2012-12-05 08:29:48 -0500

    some code cleanup

--- src/schism/load.py
+++ src/schism/load.py
@@ -6,29 +6,30 @@ import re
 import datetime
 from resolver import resolve
 
-date_pattern=re.compile("@\\d+@")
+date_pattern = re.compile("@\\d+@")
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='load a Schism document store into Solr')
-    parser.add_argument('configuration', metavar='CONFIG', type=str, 
-    help='a Resolver statement pointing to a SchismSite')
-    args=parser.parse_args()
-    site=resolve(args.configuration)
-    filenames=glob.glob('example/blog/*.json')
-    documents=[]
+    parser = argparse.ArgumentParser(
+        description='load a Schism document store into Solr')
+    parser.add_argument('configuration',
+                        metavar='CONFIG',
+                        help='a Resolver statement pointing to a SchismSite')
+    args = parser.parse_args()
+    site = resolve(args.configuration)
+    filenames = glob.glob('example/blog/*.json')
+    documents = []
     for filename in filenames:
-        path, id= os.path.split(filename)
-        parsed=json.loads(file(filename).read())
-        parsed['path']=path
-        parsed['id']=id
+        path, id = os.path.split(filename)
+        parsed = json.loads(file(filename).read())
+        parsed['path'] = path
+        parsed['id'] = id
         for key in parsed:
-            value=parsed[key]
+            value = parsed[key]
             if value.startswith('@') and date_pattern.match(value):
-                timestamp=int(value[1:-1])
-                parsed[key]=datetime.datetime.fromtimestamp(timestamp)
+                timestamp = int(value[1:-1])
+                parsed[key] = datetime.datetime.fromtimestamp(timestamp)
         documents.append(parsed)
-        
+
     print documents
     conn.add(documents)
-        
--- src/schism/storage.py
+++ src/schism/storage.py
@@ -1,16 +1,15 @@
 
+
 class LayeredStorage(object):
     def __init__(self, stores=[]):
-        self.stores=[]
-        
-        
-    def get_an_object(path_or_list):
-        if type(path_or_list)==list: #TODO: this should really accept any iterable
-            search_path=path_or_list
+        self.stores = []
+
+    def get_an_object(self, path_or_list):
+        if type(path_or_list) == list:
+            search_path = path_or_list
         else:
-            search_path=[path_or_list]
-        
-        for path in search path:
+            search_path = [path_or_list]
+
+        for path in search_path:
             for store in self.stores:
-            
-            
+                pass
